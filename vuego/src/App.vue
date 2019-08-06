6a97df60f46221ba0f011f017f5c814a69926c87
何绍府 <2734805265@163.com>
2019-08-06 11:33:08 +0800

    去哪儿项目的初始化

---
+++ vuego/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    
+    <h3>App组件</h3>
+     <p>{{message}}</p>
+      
+  </div>
+</template>
+  
+<script>
+ 
+export default {
+  name: 'App',
+
+  data(){
+return {
+       message:"helloworld",
+ }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuego/src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import fastClick from 'fastclick'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
